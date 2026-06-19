0d5762274195229f12900478952dbe14fe4fe533
jadefalco <jadefalco@gmail.com>
2026-06-18 22:39:10 -0700

    spiderfy

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,7 +2,10 @@
 
 import { useEffect, useState, useMemo, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import { getThumbnailUrl } from '@/lib/image-utils';
 import { createSpeciesMarkerIcon } from '@/lib/species-markers';
 import { createStructureMarkerIcon } from '@/lib/structure-markers';
@@ -126,7 +129,17 @@ export default function Map({ observations, userLocation, onMarkerClick, isFormO
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {markers}
+        <MarkerClusterGroup
+          spiderfyOnMaxZoom
+          spiderfyOnEveryZoom
+          zoomToBoundsOnClick={false}
+          showCoverageOnHover={false}
+          maxClusterRadius={1}
+          removeOutsideVisibleBounds
+          animate
+        >
+          {markers}
+        </MarkerClusterGroup>
       </MapContainer>
       <MapLegend isFormOpen={isFormOpen} />
     </div>
